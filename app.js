f52819c6c43b3d671f6cab0b39634e94fb7a0d5e
Iminguet <i.minguet@hotmail.com>
2023-01-07 00:51:44 +0100

    Inicio vainilla js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Vainilla JavaScript
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// addGoal = () => {
+//   const enteredVlue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredVlue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// };
+
+// buttonEl.addEventListener('click', addGoal);
+
+// Vue
