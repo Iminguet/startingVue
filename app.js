b64568c42ff5b4b23f4ef8a3b16fc4e64a9b2f0d
Iminguet <i.minguet@hotmail.com>
2023-01-07 01:07:16 +0100

    introduccion a Vue, primera programacion

--- app.js
+++ app.js
@@ -14,3 +14,18 @@
 // buttonEl.addEventListener('click', addGoal);
 
 // Vue
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: '',
+    };
+  },
+
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    },
+  },
+}).mount('#app');
